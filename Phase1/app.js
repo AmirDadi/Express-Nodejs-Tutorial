794515f660f3320059216e06268cc9484a81a2db
AmirDadi <amirreza.dadfarnia@gmail.com>
2018-05-12 07:13:40 +0430

    Api with params added

--- Phase1/app.js
+++ Phase1/app.js
@@ -7,7 +7,7 @@ const port = process.env.port || 3000;
 
 
 app.get('/', function(req, res) {
-	res.send('<html> <head> First Page </head> <body> <h1> Hello world </h1> </body> </html>');
+	res.send('<html> <head> <h1> First Page </h1> </head> <body> Hello world </body> </html>');
 });
 
 app.get('/name', function(req, res) {
@@ -17,5 +17,9 @@ app.get('/name', function(req, res) {
 	});
 });
 
+app.get('/home/:name/:lastName', function(req, res) {
+	res.send('<html> <head> <h1> First Page </h1> </head>' + 
+		'<body> Hello ' + req.params.name + ' ' + req.params.lastName  + '</body> </html>')
+});
 app.listen(port);
 console.log('Listening on port: ' + port)
